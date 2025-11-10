3cdda03372f95c5294cd29b4ddc12941d12182e0
Dominic <dominicprestia0@gmail.com>
2025-11-10 14:58:37 -0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Dominic Portfolio Website</h1>
+        <p>🚧 Under Construction 🚧</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
